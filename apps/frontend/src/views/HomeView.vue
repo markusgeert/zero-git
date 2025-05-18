6fbe74ee54989c727cbc6818f8809e0c8ac9a2c8
Max Stevens <maxstevens2708@gmail.com>
2025-05-18 18:16:04 +0200

    bun fix

--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -44,7 +44,9 @@ async function addRepo() {
 	z.value.mutate.reposTable.create({
 		id: nanoid(),
 		orgId: nanoid(),
-		orgGithubId: faker.number.int({ min: 1_000_000, max: 9_999_999 }).toString(),
+		orgGithubId: faker.number
+			.int({ min: 1_000_000, max: 9_999_999 })
+			.toString(),
 		orgName: faker.internet.username(),
 		githubId: faker.number.int({ min: 1_000_000, max: 9_999_999 }).toString(),
 		visibility: "public",
