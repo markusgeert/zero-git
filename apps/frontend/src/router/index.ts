090775e6a1f484eb025e739e440c179729c9fcb2
Max Stevens <maxstevens2708@gmail.com>
2025-05-20 16:39:11 +0200

    redirect user on error

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -50,4 +50,13 @@ const router = createRouter({
 	],
 });
 
+router.onError((error, to) => {
+	if (
+		error.message.includes("Failed to fetch dynamically imported module") ||
+		error.message.includes("Importing a module script failed")
+	) {
+		window.location = to.fullPath;
+	}
+});
+
 export default router;
